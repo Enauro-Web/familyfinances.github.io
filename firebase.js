ff13cba2fe2b691932e28ef91792c0b677b724f4
Rafael OrtizPostigo <ESORTIZPOSTR@tetrapak.com>
2022-08-31 14:32:07 +0200

    20220831

---
+++ firebase.js
@@ -0,0 +1,151 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+//import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
+import { 
+    getFirestore,
+    collection,
+    addDoc,
+    deleteDoc,
+    getDocs,
+    onSnapshot,
+    doc,
+    getDoc,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyDWnziUkQpnw56JNoTw87YJbH5TopRHmD0",
+authDomain: "family-finance-c0b7a.firebaseapp.com",
+projectId: "family-finance-c0b7a",
+storageBucket: "family-finance-c0b7a.appspot.com",
+messagingSenderId: "9813308230",
+appId: "1:9813308230:web:14962347efccb8aa8c6b47",
+measurementId: "G-7GEVZ2ZS4W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+const db = getFirestore();
+
+//EXPENSES
+
+export const saveExpense = (title, date, description, account, quantity, currency) => {
+    //console.log(title, description, account, quantity)
+    addDoc(collection(db,'expenses'), {title,date,description,account,quantity, currency})
+    updateBalace();
+}
+
+export const getExpenses = async () => {
+    var results = await getDocs(collection(db,'expenses'))
+    return results;
+}
+
+export const onGetExpenses = (callback) => {
+    onSnapshot(collection(db, 'expenses'), callback);
+}
+
+export const deleteExpense = (id) => {
+    deleteDoc(doc(db,'expenses',id));
+    updateBalace();
+}
+
+export const editExpense = async (id) => {
+    return await getDoc(doc(db,'expenses',id));
+}
+
+export const updateExpense = async (id, newFields) => {
+    await updateDoc(doc(db, 'expenses', id), newFields);
+}
+
+//export {onSnapshot, collection,db};
+
+
+// INCOMES
+
+export const saveIncome = (title, date, description, account, quantity, currency) => {
+    //console.log(title, description, account, quantity)
+    addDoc(collection(db,'incomes'), {title,date, description,account,quantity, currency})
+    updateBalace();
+}
+
+export const onGetIncomes = (callback) => {
+    onSnapshot(collection(db, 'incomes'), callback);
+}
+
+export const deleteIncome = (id) => {
+    deleteDoc(doc(db,'incomes',id));
+    updateBalace();
+}
+
+export const editIncome = async (id) => {
+    return await getDoc(doc(db,'incomes',id));
+}
+
+export const updateIncome = async (id, newFields) => {
+    await updateDoc(doc(db, 'incomes', id), newFields);    
+}
+
+// BALANCE
+
+export const updateBalace = async () => {
+    var expResults = await getDocs(collection(db,'expenses'))
+    let totalExpING = 0;
+    let totalExpSantander = 0;
+    let totalExpAdvCash = 0;
+    let totalExpBitBox = 0;    
+    let totalExpINGFondo = 0;    
+    expResults.forEach(exp => {
+        const account = exp.data().account;
+        if(account === 'ING'){
+            //console.log(exp.data().quantity)
+            totalExpING += parseFloat(exp.data().quantity);
+        }else if(account === 'Santander'){
+            totalExpSantander += parseFloat(exp.data().quantity);
+        }else if(account === 'ADVCash'){
+            totalExpAdvCash += parseFloat(exp.data().quantity);
+        }else if(account === 'ING-Fondo'){
+            totalExpINGFondo += parseFloat(exp.data().quantity);
+        }else if(account === 'BitBox'){
+            totalExpBitBox += parseFloat(exp.data().quantity);
+        }        
+
+    });
+    //console.log(totalExpING)
+
+    var incResults = await getDocs(collection(db,'incomes'))
+    let totalIncING = 0;
+    let totalIncSantander = 0;
+    let totalIncAdvCash = 0;
+    let totalIncBitBox = 0;    
+    let totalIncINGFondo = 0;    
+    incResults.forEach(inc => {
+        const account = inc.data().account;
+        if(account === 'ING'){
+            totalIncING += parseFloat(inc.data().quantity);
+        }else if(account === 'Santander'){
+            totalIncSantander += parseFloat(inc.data().quantity);
+        }else if(account === 'ADVCash'){
+            totalIncAdvCash += parseFloat(inc.data().quantity);
+        }else if(account === 'ING-Fondo'){
+            totalIncINGFondo += parseFloat(inc.data().quantity);
+        }else if(account === 'BitBox'){
+            totalIncBitBox += parseFloat(inc.data().quantity);
+        }        
+
+    });
+    //console.log(totalIncING)
+    //console.log("Balance ING:", totalIncING - totalExpING);
+    //console.log("Balance Santander:", totalIncSantander - totalExpSantander);
+    const divBalances = document.getElementById('balances');
+    divBalances.innerHTML = `
+    <h3 class="h5">Balances</h3>
+    <p>ING: <strong>${totalIncING - totalExpING}</strong></p>
+    <p>Santander: <strong>${totalIncSantander - totalExpSantander}</strong></p>
+    `;
+}
---
+++ index.js
@@ -0,0 +1,233 @@
+import {
+    saveExpense,
+    getExpenses,
+    onGetExpenses, 
+    deleteExpense,
+    editExpense,
+    updateExpense,
+    saveIncome,
+    onGetIncomes, 
+    deleteIncome,
+    editIncome,
+    updateIncome,
+    updateBalace,
+} from './firebase.js'
+const container = document.getElementById('Expense-container');
+const containerIncome = document.getElementById('Income-container');
+const newExpenseForm = document.getElementById('newExpense-form');
+const newIncomeForm = document.getElementById('newIncome-form');
+
+let editExpStatus = false;
+let editIncStatus = true;
+let id = '';
+
+window.addEventListener('DOMContentLoaded', async ()=>{
+    //const querySnapshot = await getExpenses();
+    updateBalace();
+    onGetExpenses((querySnapshot) => {
+        let html = '<div class="row">'
+        querySnapshot.forEach(doc => {
+            //console.log(doc.data())
+            const expense = doc.data();
+            html += `
+            <div class="col-md-3">
+                <div class="card card-body mt-2 border-primary">
+                    <h3 class="h5">${expense.title}</h3>
+                    <p>EXPENSE</p>
+                    <p>${expense.date}</p>
+                    <p>${expense.description}</p>
+                    <p>${expense.account}</p>
+                    <p>${expense.quantity} ${expense.currency}</p>
+                    <div class="display-flex">
+                        <button class='btn btn-primary btn-delete' data-id='${doc.id}'>Delete</button>
+                        <button class='btn btn-secondary btn-edit' data-id='${doc.id}'>Edit</button>
+                    </div>
+                </div>
+            </div>
+            `
+        });
+        html += '</div>'
+    
+        container.innerHTML = html;
+        const btnsDelete = container.querySelectorAll('.btn-delete');
+        btnsDelete.forEach(btn => {
+            /*
+            btn.addEventListener('click', (e) => {
+                console.log('Deleting', e.target.dataset.id);
+            })
+            */
+            btn.addEventListener('click', ({target: {dataset}}) => {
+                //console.log(dataset.id);
+                deleteExpense(dataset.id);
+            })
+        })
+        const btnsEdit = container.querySelectorAll('.btn-edit');
+        btnsEdit.forEach(btn => {
+            btn.addEventListener('click', async ({target: {dataset}}) => {
+                id = dataset.id;
+                const doc = await editExpense(dataset.id);
+                newExpenseForm['newExpense-title'].value = doc.data().title;
+                newExpenseForm['newExpense-description'].value = doc.data().description;
+                newExpenseForm['newExpense-date'].value = doc.data().date;
+                newExpenseForm['newExpense-account'].value = doc.data().account;
+                newExpenseForm['newExpense-quantity'].value = doc.data().quantity;
+                newExpenseForm['newExpense-currency'].value = doc.data().currency;
+
+                editExpStatus = true;
+                newExpenseForm['btn-newExpense-save'].innerText = 'Update';
+            })
+        })
+    });
+    
+    onGetIncomes((querySnapshot) => {
+        let html = '<div class="row">'
+        querySnapshot.forEach(doc => {            
+            const income = doc.data();
+            html += `
+            <div class="col-md-3">
+                <div class="card card-body mt-2 border-primary">
+                    <h3>${income.title}</h3>
+                    <p>INCOME</p>
+                    <p>${income.date}</p>
+                    <p>${income.description}</p>
+                    <p>${income.account}</p>
+                    <p>${income.quantity} ${income.currency}</p>
+                    <div>
+                        <button class='btn btn-primary btn-delete' data-id='${doc.id}'>Delete</button>
+                        <button class='btn btn-secondary btn-edit' data-id='${doc.id}'>Edit</button>
+                    </div>
+                </div>
+            </div>
+            `
+        });
+        html += '</div>'
+        containerIncome.innerHTML = html;
+        const btnsDelete = containerIncome.querySelectorAll('.btn-delete');
+        btnsDelete.forEach(btn => {
+            btn.addEventListener('click', ({target: {dataset}}) => {                
+                deleteIncome(dataset.id);
+            })
+        })
+        const btnsEdit = containerIncome.querySelectorAll('.btn-edit');
+        btnsEdit.forEach(btn => {
+            btn.addEventListener('click', async ({target: {dataset}}) => {   
+                id = dataset.id;             
+                const doc = await editIncome(dataset.id);
+                newIncomeForm['newIncome-title'].value = doc.data().title;
+                newIncomeForm['newIncome-description'].value = doc.data().description;
+                newIncomeForm['newIncome-date'].value = doc.data().date;
+                newIncomeForm['newIncome-account'].value = doc.data().account;
+                newIncomeForm['newIncome-quantity'].value = doc.data().quantity;
+                newIncomeForm['newIncome-currency'].value = doc.data().currency;
+
+                editIncStatus = true;
+                newIncomeForm['btn-newIncome-save'].innerText = 'Update';
+
+                
+            })
+        })
+    });   
+
+
+});
+
+newExpenseForm.addEventListener('submit', e => {
+    e.preventDefault();    
+    const newExpenseTitle = newExpenseForm['newExpense-title']
+    const newExpenseDate = newExpenseForm['newExpense-date']
+    const newExpenseDescription = newExpenseForm['newExpense-description']
+    const newExpenseAccount = newExpenseForm['newExpense-account']
+    const newExpenseQuantity = newExpenseForm['newExpense-quantity']
+    const newExpenseCurrency = newExpenseForm['newExpense-currency']
+
+    if(editExpStatus){
+        updateExpense(id, {
+            title: newExpenseTitle.value,
+            date: newExpenseDate.value,
+            description: newExpenseDescription.value,
+            account: newExpenseAccount.value, 
+            quantity: newExpenseQuantity.value,
+            currency: newExpenseCurrency.value
+        });
+        updateBalace();
+    } else {
+        saveExpense(
+            newExpenseTitle.value,
+            newExpenseDate.value,
+            newExpenseDescription.value,
+            newExpenseAccount.value, 
+            newExpenseQuantity.value,
+            newExpenseCurrency.value
+        );
+    }
+    
+    newExpenseForm.reset();
+    editExpStatus = false;
+    newExpenseForm['btn-newExpense-save'].innerText = 'Save';
+});
+
+newIncomeForm.addEventListener('submit', e => {
+    e.preventDefault();    
+    const newIncomeTitle = newIncomeForm['newIncome-title']
+    const newIncomeDate = newIncomeForm['newIncome-date']
+    const newIncomeDescription = newIncomeForm['newIncome-description']    
+    const newIncomeAccount = newIncomeForm['newIncome-account']
+    const newIncomeQuantity = newIncomeForm['newIncome-quantity']
+    const newIncomeCurrency = newIncomeForm['newIncome-currency']
+
+    if(editIncStatus){
+        updateIncome(id, {
+            title: newIncomeTitle.value,
+            date: newIncomeDate.value,
+            description: newIncomeDescription.value,
+            account: newIncomeAccount.value, 
+            quantity: newIncomeQuantity.value,
+            currency: newIncomeCurrency.value
+        });
+        updateBalace();
+    } else {
+        saveIncome(
+            newIncomeTitle.value,
+            newIncomeDate.value,
+            newIncomeDescription.value,
+            newIncomeAccount.value, 
+            newIncomeQuantity.value,
+            newIncomeCurrency.value
+        );
+    }
+
+    newIncomeForm.reset();
+    editIncStatus = false;
+});
+
+/*
+
+const divExp = document.getElementById('divExpenses');
+const divInc = document.getElementById('divIncomes');
+const tabExpensesBtn = document.getElementById('expenses-tab');
+const tabIncomesBtn = document.getElementById('incomes-tab');
+
+divInc.style.visibility = 'hidden';
+divExp.style.visibility = 'visible';
+
+
+tabExpensesBtn.addEventListener('click', e => {
+    e.preventDefault();
+    //console.log('CLICKED expenses');
+    divInc.style.visibility = 'hidden';
+    divExp.style.visibility = 'visible';
+    tabIncomesBtn.classList.remove('active');
+    tabExpensesBtn.classList.add('active');
+});
+
+tabIncomesBtn.addEventListener('click', e => {
+    e.preventDefault();
+    //console.log('CLICKED incomes');
+    divInc.style.visibility = 'visible';
+    divExp.style.visibility = 'hidden';
+    tabIncomesBtn.classList.add('active');
+    tabExpensesBtn.classList.remove('active');
+
+});
+
+*/
