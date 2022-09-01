c8de4d3dd4ff671134099b8ec8728b46b400203d
Rafael OrtizPostigo <ESORTIZPOSTR@tetrapak.com>
2022-09-01 10:00:06 +0200

    20220901_01

--- firebase.js
+++ firebase.js
@@ -15,6 +15,12 @@ import {
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import {
+    getAuth, 
+    createUserWithEmailAndPassword
+ } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js"
+
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -149,3 +155,25 @@ export const updateBalace = async () => {
     <p>Santander: <strong>${totalIncSantander - totalExpSantander}</strong></p>
     `;
 }
+
+//AUTHENTICATION
+
+const signUpForm = document.getElementById('signUpForm');
+const auth = getAuth();
+
+signUpForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const signUpEmail = document.getElementById('signup-email').value;
+    const signUpPassw = document.getElementById('signup-password').value;    
+    
+    createUserWithEmailAndPassword(auth, signUpEmail, signUpPassw)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(`User signed in ${user}`);
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+})
